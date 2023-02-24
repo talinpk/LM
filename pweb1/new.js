04b0a4d321f10a505afb97401be6af9bac24ed0f
talinpk <96902115+talinpk@users.noreply.github.com>
2023-02-24 13:34:12 -0300

    .

--- pweb1/new.js
+++ pweb1/new.js
@@ -3,3 +3,21 @@ let matriculas = [1,3,5,7,8,9,10,15];
 matriculas.unshift(2,4,6);
 
 matriculas.forEach(matricula => console.log(matricula));
+
+
+let usuarios = [
+    {
+"login": "u1",
+        "nome": "Carlos"
+    },
+    {
+"login": "u2",
+        "nome": "Joaquim"
+    },
+    {
+"login": "u3",
+        "nome": "Maria"
+    }
+];
+
+console.log(usuarios[0]);
