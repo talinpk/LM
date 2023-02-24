8f1a75cb6bb3542de988f16f97a67bb2f7140844
talinpk <96902115+talinpk@users.noreply.github.com>
2023-02-23 21:46:31 -0300

    .

--- pweb1/new.js
+++ pweb1/new.js
@@ -1,3 +1,11 @@
-for (let i = 0; i <= 7; i ++) {
-    console.log("#");
-  }
+let matriculas = [1,3,5,7,8,9,10,15];
+
+matriculas[0]; // 1o elemento - valor 1
+matriculas[matriculas.length - 1]; // Último elemento - valor 15
+matriculas[5]; // 6o elemento - valor 9
+
+console.log(matriculas)
+
+for (let matricula of matriculas) {
+    console.log(matricula);
+    }
