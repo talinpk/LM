eca422b9216282b6d16ef02934e64d4f38475730
talinpk <96902115+talinpk@users.noreply.github.com>
2023-02-23 23:46:50 -0300

    .

--- pweb1/new.js
+++ pweb1/new.js
@@ -1,13 +1,5 @@
 let matriculas = [1,3,5,7,8,9,10,15];
 
-matriculas[0]; // 1o elemento - valor 1
-matriculas[matriculas.length - 1]; // Último elemento - valor 15
-matriculas[5]; // 6o elemento - valor 9
-
-console.log(matriculas)
-
-for (let matricula of matriculas) {
-    console.log(matricula);
-    }
+matriculas.unshift(2,4,6);
 
 matriculas.forEach(matricula => console.log(matricula));
